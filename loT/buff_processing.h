2215e45f98b0019d281cb4e35978d61af0cd5e2f
Yoga-bf <cbf98012@mail.ustc.edu.cn>
2021-04-21 03:44:51 -0700

    Iot2.0

--- loT/buff_processing.h
+++ loT/buff_processing.h
@@ -3,7 +3,25 @@
 
 
 #include "loT.h"
+const int  N            = 1024;
 
 int buff_generate(char * buff, int Type, char * message, int n, loT & SloT, loT DloT = loT(DEFAULT_TYPE));
+struct shared_buff
+{
+    char                buff[N];
+    bool                ready;
+    int                 buff_len;
+    mutex               mtx;
+    condition_variable  cv;
+};
+
+// #ifndef LOCAL_SHARED_
+// extern shared_buff shared;
+// #endif
+
+
+void Socket_Send(int & socketfd_send, loT & Slot, struct shared_buff *shared);
+void Socket_Recv(int & socketfd_recv, loT & Slot, struct shared_buff *shared);
+
 
 #endif
