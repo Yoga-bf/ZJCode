5442e909ce47e33801862b3d5a843e57eb6f7952
Yoga-bf <cbf98012@mail.ustc.edu.cn>
2021-04-26 03:45:20 -0700

    manage

--- Manage/Com_thread.cpp
+++ Manage/Com_thread.cpp
@@ -3,11 +3,13 @@
 #include "loT.h"
 #include "wrap.h"
 #include "Com_thread.h"
+#include "pcap_send.h"
 
 using namespace std;
 
 void* Recv_loT_Signup(int server_socket_fd, loTDatabase* AllloT)
 {
+    //Process the data sent by the loT
     int AcceptFd;
     while (1) {
         AcceptFd = Accept(server_socket_fd, NULL, NULL);
@@ -31,6 +33,7 @@ void* Recv_loT_Signup(int server_socket_fd, loTDatabase* AllloT)
 
 void* Com_with_loT(loTMetadata* myloT, loTDatabase* AllloT)
 {
+    //Communication with IoT 
     int size;
     char buffer[N] = {0};
     while(1) {
@@ -49,11 +52,41 @@ void* Com_with_loT(loTMetadata* myloT, loTDatabase* AllloT)
             }
             AllloT->mtx.unlock();
         } else {
+            loTMetadata slot;
+            loTMetadata dlot;
+            int k = MessageProcessing(buffer, size, Com, &slot, &dlot);
+            if (k == -1 || slot.ID != myloT->ID) {
+                cout << "the packet which loT send is wrong" << endl;
+                continue;
+            }
             myloT->buff_len = size;
             memcpy(myloT->buff, buffer, N);
             //how to deal with this package
             //may be the des is not belong the manage, otherwise.
             //may be i need a function to send the message to the lot of the manage.
+            int desfd;
+            AllloT->mtx.lock_shared();
+            for (auto it = AllloT->loTDB.begin(); it != AllloT->loTDB.end(); it++) {
+                if (it->ID == dlot.ID) {
+                    k = -1;
+                    desfd = it->socketfd;
+                    break;
+                }
+            }
+            AllloT->mtx.unlock_shared();
+            if (k == -1) {
+                //the lot is belong manage
+                size = Write(desfd, buffer, size);
+            } else {
+                //the lot is not belong manage
+                pcap_send(buffer, NULL, DST_IP, 1, size);
+            }
         }
     }
+}
+
+void* Pcap_Receive()
+{
+    //Process the data sent by the switch
+    pcap_receive();
 }
--- Manage/net.h
+++ Manage/net.h
@@ -11,7 +11,6 @@ const static char Com = '2';
 const int N = 1024;
 void SocketMake(struct sockaddr_in * ser_addr, int * server_socket_fd, int PORT, const char *Myip);
 int MessageProcessing(char *message, int size, char Type, struct loTMetadata *SloT, struct loTMetadata *DloT = NULL);
-int SendMessage(loTMetadata* DloT);
 
 static std::map<char, int> TypeLength = {{'0', 24}, {'1', 17}, {'2', 16}};
 
