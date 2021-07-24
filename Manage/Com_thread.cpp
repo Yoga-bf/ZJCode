462c3bcb97d848d5b75c80897968564c7cda640f
Yoga-bf <cbf98012@mail.ustc.edu.cn>
2021-07-24 03:48:16 -0700

    finish

--- Manage/Com_thread.cpp
+++ Manage/Com_thread.cpp
@@ -31,7 +31,7 @@ void* Recv_loT_Signup(int server_socket_fd)
         int k = MessageProcessing(buffer, size, SignUp, &loTs[threadnum]);
         if (k == -1)
             continue;
-
+        cout << "The lot " << loTs[threadnum].ID << " has connected" << endl;
         loTs[threadnum].socketfd = AcceptFd;
         //loTs.push_back(SID);
         AllloT.mtx.lock();
@@ -52,20 +52,20 @@ void* Com_with_loT(loTMetadata* myloT)
     char buffer[N] = {0};
     cout << "Com with loT " << myloT->ID << endl;
     
+    //sign in
+    buffer[0] = '0';
+    buffer[1] = myloT->type;
+    memcpy(&buffer[2], myloT->ID, myloT->length);
+    size = myloT->length + 2;
+    pcap_send(buffer, NULL, DST_IP, 1, size);
+
     while(1) {
         memset(buffer, 0, N*sizeof(char));
         
-        //sign in
-        buffer[0] = '0';
-        buffer[1] = myloT->type;
-        memcpy(&buffer[2], myloT->ID, myloT->length);
-        size = myloT->length + 2;
-        pcap_send(buffer, NULL, DST_IP, 1, size);
-
         size = Read(myloT->socketfd, buffer, N);
         cout << "lot send something" << endl;
         if (size <= 0) {
-            cout << myloT->ID << " has been sign down" << endl;
+            
 
             //send the sign down package
             memset(buffer, 0, N*sizeof(char));
@@ -77,19 +77,21 @@ void* Com_with_loT(loTMetadata* myloT)
             //delete the data of lot
             AllloT.mtx.lock();
             for (auto it = AllloT.loTDB.begin(); it != AllloT.loTDB.end(); it++) {
-                if (it->ID == myloT->ID) {
+                if (strcmp(it->ID, myloT->ID) == 0) {
                     it = AllloT.loTDB.erase(it);
                     break;
                 }
             }
             AllloT.mtx.unlock();
+            cout << myloT->ID << " has been sign down" << endl;
+            break;
 
         } else {
             loTMetadata slot;
             loTMetadata dlot;
             //something wrong with this function
             int k = MessageProcessing(buffer, size, Com, &slot, &dlot);
-            if (k == -1 || strcmp(slot.ID, myloT->ID) != 0) {
+            if (k == -1 || (strcmp(slot.ID, myloT->ID) != 0)) {
                 cout << "the packet which loT send is wrong" << endl;
                 continue;
             }
@@ -98,17 +100,18 @@ void* Com_with_loT(loTMetadata* myloT)
             cout << myloT->ID << " send " << buffer << endl;
             //whether the des lot is belonging of manage
             int desfd;
-            AllloT.mtx.lock_shared();
+            int flag = 0;
+            AllloT.mtx.try_lock_shared();
             for (auto it = AllloT.loTDB.begin(); it != AllloT.loTDB.end(); it++) {
-                if (it->ID == dlot.ID) {
-                    k = -1;
+                if (strcmp(it->ID, dlot.ID) == 0) {
+                    flag = -1;
                     desfd = it->socketfd;
                     break;
                 }
             }
             AllloT.mtx.unlock_shared();
 
-            if (k == -1) {
+            if (flag == -1) {
                 //the lot is belong manage
                 size = Write(desfd, buffer, size);
             } else {
@@ -117,12 +120,13 @@ void* Com_with_loT(loTMetadata* myloT)
             }
         }
     }
-    
+    return NULL;
 }
 
 void* Pcap_Receive()
 {
     //Process the data sent by the switch
+    cout << "wait for others manage send message" << endl;
     int k = pcap_receive();
-    
+    return NULL;
 }
